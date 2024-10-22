e7c6abb07c8542b3c3e91f89de1934cdbee88111
Winlypse <nts3@o2.pl>
2024-10-22 14:39:10 +0200

    Changes!

--- src/main/kotlin/winlyps/noFireTick/NoFireTick.kt
+++ src/main/kotlin/winlyps/noFireTick/NoFireTick.kt
@@ -6,9 +6,14 @@ class NoFireTick : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("doFireTick", "false")
+        }
+        logger.info("NoFireTick plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("NoFireTick plugin has been disabled.")
     }
 }
