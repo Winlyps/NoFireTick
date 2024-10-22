beacaba796f807cd4cf55951e6d35dac3a7e689d
Winlypse <nts3@o2.pl>
2024-10-22 14:38:53 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/noFireTick/NoFireTick.kt
@@ -0,0 +1,14 @@
+package winlyps.noFireTick
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class NoFireTick : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
